6845c78a864d27d779962484280a28c0238c2745
Gabriel Loiola <jgabriel.loiola@gmail.com>
2025-03-26 08:07:00 -0300

    Mudança de pasta do projeto

--- build/pico-sdk/src/cmake_install.cmake
+++ build/pico-sdk/src/cmake_install.cmake
@@ -39,9 +39,9 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/gabri/OneDrive/Desktop/repositorio/coresLed/build/pico-sdk/src/common/cmake_install.cmake")
-  include("C:/Users/gabri/OneDrive/Desktop/repositorio/coresLed/build/pico-sdk/src/rp2_common/cmake_install.cmake")
-  include("C:/Users/gabri/OneDrive/Desktop/repositorio/coresLed/build/pico-sdk/src/rp2040/cmake_install.cmake")
+  include("C:/Users/gabri/OneDrive/Desktop/projetosRaspberry/coresLed/build/pico-sdk/src/common/cmake_install.cmake")
+  include("C:/Users/gabri/OneDrive/Desktop/projetosRaspberry/coresLed/build/pico-sdk/src/rp2_common/cmake_install.cmake")
+  include("C:/Users/gabri/OneDrive/Desktop/projetosRaspberry/coresLed/build/pico-sdk/src/rp2040/cmake_install.cmake")
 
 endif()
 
